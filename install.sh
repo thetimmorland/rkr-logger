4fc2d53217e443c6f893a7194a0dc3f40f199ed7
Timothy John Morland <thetimmorland@gmail.com>
2020-09-13 12:08:49 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,4 +10,5 @@ curl -sL https://github.com/thetimmorland/rkr-logger/tarball/master\
 # modify /boot/config.txt (see https://stackoverflow.com/questions/20267910/how-to-add-a-line-in-sed-if-not-match-is-found/30736034)
 
 # install apt dependencies
-apt-get install can-utils
+apt-get update
+apt-get install -y can-utils
